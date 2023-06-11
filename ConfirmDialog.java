e8745572da9ba6f1655c0941460cbfb4e6569329
AvijitDasAvi <61002222+AvijitDasAvi@users.noreply.github.com>
2023-06-11 23:23:41 +0600

    ConfirmDialog.java

--- ConfirmDialog.java
+++ ConfirmDialog.java
@@ -6,6 +6,7 @@ public class ConfirmDialog {
     public static void main(String[] args) {
         
         int choice =JOptionPane.showConfirmDialog(null,"Do you want to quit ?","User choose",JOptionPane.YES_NO_OPTION);
+        //Use 5 parameters
         if (choice == JOptionPane.YES_OPTION) {
             System.exit(0);
         }
